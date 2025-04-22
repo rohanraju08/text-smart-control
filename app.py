bc9c6edf30e53d707f5f3b8e9c446bca8088e671
rohanraju08 <rohan.v.r.raju@gmail.com>
2025-04-22 18:21:58 +0530

    Update app.py
    port set to 5000 from local for render to work

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def home():
     return "🌐 WhatsApp Smart Controller is LIVE ✅", 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
